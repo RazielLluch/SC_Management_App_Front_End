cae94762448fc6628a7fea054310965ee384f9e8
Josiah Raziel S. Lluch <josiahraziel.lluch@g.msuiit.edu.ph>
2026-08-18 03:46:32 +0800

    Temporarily set signup to student form only

--- app/(auth)/signup/page.tsx
+++ app/(auth)/signup/page.tsx
@@ -1,10 +1,12 @@
-import { SignupForm } from "@/app/(auth)/signup/components/signup-form"
+import {SignupStudentForm} from "@/app/(auth)/signup/components/signup-student-form";
+import {SignupFacultyForm} from "@/app/(auth)/signup/components/signup-faculty-form";
+import {SignupOrgForm} from "@/app/(auth)/signup/components/signup-org-form";
 
 export default function Page() {
     return (
         <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
             <div className="w-full max-w-sm">
-                <SignupForm />
+                <SignupStudentForm />
             </div>
         </div>
     )
