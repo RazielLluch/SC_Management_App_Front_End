7caa02e9408f5be598a473569e47cff2e36f994b
Josiah Raziel S. Lluch <josiahraziel.lluch@g.msuiit.edu.ph>
2026-09-14 03:13:42 +0800

    set indents to 2 spaces size

--- app/(auth)/auth/confirm/route.ts
+++ app/(auth)/auth/confirm/route.ts
@@ -4,44 +4,44 @@ import { type NextRequest, NextResponse } from 'next/server'
 import { createClient } from '@/utils/supabase/server'
 
 export async function GET(request: NextRequest) {
-    const { searchParams } = new URL(request.url)
-    const token_hash = searchParams.get('token_hash')
-    const type = searchParams.get('type') as EmailOtpType | null
-    const next = searchParams.get('next') ?? '/'
+  const { searchParams } = new URL(request.url)
+  const token_hash = searchParams.get('token_hash')
+  const type = searchParams.get('type') as EmailOtpType | null
+  const next = searchParams.get('next') ?? '/'
 
-    const redirectTo = request.nextUrl.clone()
-    redirectTo.pathname = next
-    redirectTo.searchParams.delete('token_hash')
-    redirectTo.searchParams.delete('type')
+  const redirectTo = request.nextUrl.clone()
+  redirectTo.pathname = next
+  redirectTo.searchParams.delete('token_hash')
+  redirectTo.searchParams.delete('type')
 
-    console.log("Callback URL:", request.url);
-    console.log("token_hash:", token_hash);
-    console.log("type:", type);
-    console.log("next:", next);
+  console.log("Callback URL:", request.url);
+  console.log("token_hash:", token_hash);
+  console.log("type:", type);
+  console.log("next:", next);
 
-    if (token_hash && type) {
-        const supabase = await createClient()
+  if (token_hash && type) {
+    const supabase = await createClient()
 
-        // const { error } = await supabase.auth.verifyOtp({
-        //     type,
-        //     token_hash,
-        // })
+    // const { error } = await supabase.auth.verifyOtp({
+    //     type,
+    //     token_hash,
+    // })
 
-        const { data, error } = await supabase.auth.verifyOtp({
-            type,
-            token_hash,
-        });
+    const { data, error } = await supabase.auth.verifyOtp({
+      type,
+      token_hash,
+    });
 
-        console.log("verifyOtp data:", data);
-        console.log("verifyOtp error:", error);
+    console.log("verifyOtp data:", data);
+    console.log("verifyOtp error:", error);
 
-        if (!error) {
-            redirectTo.searchParams.delete('next')
-            return NextResponse.redirect(redirectTo)
-        }
+    if (!error) {
+      redirectTo.searchParams.delete('next')
+      return NextResponse.redirect(redirectTo)
     }
+  }
 
-    // return the user to an error page with some instructions
-    redirectTo.pathname = '/error'
-    return NextResponse.redirect(redirectTo)
+  // return the user to an error page with some instructions
+  redirectTo.pathname = '/error'
+  return NextResponse.redirect(redirectTo)
 }
